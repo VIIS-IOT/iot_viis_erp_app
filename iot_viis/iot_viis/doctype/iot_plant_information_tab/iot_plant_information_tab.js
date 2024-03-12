49ed3b0b3710b7245b57372f254a9ac27e14927d
nguyenhieu11 <nt.hieu.1911@gmail.com>
2024-03-12 02:17:08 +0000

    add missing doctype

---
+++ iot_viis/iot_viis/doctype/iot_plant_information_tab/iot_plant_information_tab.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Pyroject and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('iot_plant_information_tab', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ iot_viis/iot_viis/doctype/iot_virtual_device/iot_virtual_device.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Pyroject and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('iot_virtual_device', {
+	// refresh: function(frm) {
+
+	// }
+});
